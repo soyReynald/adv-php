4b58a89f1bac904805d9dab7730040ebd2beeb8f
Reynald Ramirez <soyreynald@outlook.com>
2019-10-14 23:33:07 -0400

    Improvement

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <title>CMS with PHP</title>
     <meta name="charset" content="utf-8">
     <link rel="stylesheet" href="styles.css">
-    <script type="text/javascript" src="scripts.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 </head>
 <body>
     <!-- Project oriented to create our Own CMS with PHP -->
-        
+    
 </body>
 </html>
