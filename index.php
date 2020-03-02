06b0cdeec20eece32db708523a4ae52b54bbdcb2
Reynald Rodriguez <soyreynald@outlook.com>
2020-03-01 22:46:23 -0400

    Updating Connection to the DB

--- index.php
+++ index.php
@@ -8,6 +8,21 @@
 </head>
 <body>
     <!-- Project oriented to create our Own CMS with PHP -->
-    
+    <?php
+        $db['DB_HOST'] = "localhost";
+        $db['DB_USER'] = "root";
+        $db['DB_PASSWORD'] = "";
+        $db['DB_NAME'] = "cs_";
+        
+        foreach($db as $key => $value){
+            define(strtoupper($key), $value);
+        }
+
+        $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if($connection){
+            echo "Connection success";
+        }
+    ?>
 </body>
 </html>
---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+$db['DB_HOST'] = "localhost";
+$db['DB_USER'] = "root";
+$db['DB_PASSWORD'] = "";
+$db['DB_NAME'] = "cs_";
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+?>
