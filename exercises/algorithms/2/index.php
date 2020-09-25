c2de065a8231a1f8b41ade4b3ef065add9d595dd
Reynald Ramirez <soyreynald@outlook.com>
2020-09-25 15:01:21 -0400

    New algorithm - 2

---
+++ exercises/algorithms/2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array_push() & json_encode()</title>
+</head>
+<body>
+    <div class="ex">
+        <h1>Algoritmo 2</h1>
+        <p>Se tiene una tabla en MySQL, se desea convertir los datos desde la tabla a un array y luego a un JSON para ser leidos posteriormente por un 
+        JavaScript</p>
+    </div>
+</body>
+</html>
