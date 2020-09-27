dd00ee36276db9a26f549f86ba4b3b3cf3cc8552
Reynald Ramirez <soyreynald@outlook.com>
2020-09-27 18:15:40 -0400

    What I have tried

--- exercises/algorithms/2/js/app.js
+++ exercises/algorithms/2/js/app.js
@@ -1,4 +1,5 @@
 function leer(user){
+    
     $.ajax({
         method: "GET",
         url: "API/clientes.php",
@@ -15,4 +16,34 @@ function leer(user){
             );
         })
     })
+    /*
+    var request = new XMLHttpRequest();
+    request.open('GET', 'API/clientes.php', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    var data = {usuario: user}
+    
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+        // Success!
+        var obj = JSON.parse(this.response);
+        obj.forEach(element => {
+            var body = document.querySelector('table tbody');
+            body.append(
+                "<tr>"+
+                    "<td>"+element.nombre+'</td>'+
+                "</tr>"
+            );
+        });
+        } else {
+            alert("An error");
+    
+        }
+    };
+
+    request.send(data);
+    
+    /*** Error: Uncaught SyntaxError: Unexpected end of JSON input
+     * at JSON.parse (<anonymous>) at XMLHttpRequest.request.onload
+     * 
+     */
 }
