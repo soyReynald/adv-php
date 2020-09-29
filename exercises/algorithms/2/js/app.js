4787255e8249493c8d4219e61bcc11f4fb86df0c
Reynald Ramirez <soyreynald@outlook.com>
2020-09-29 16:28:57 -0400

    Resource

--- exercises/algorithms/2/js/app.js
+++ exercises/algorithms/2/js/app.js
@@ -16,36 +16,5 @@ function leer(user){
             );
         })
     })
-    /*
-    var request = new XMLHttpRequest();
-    request.open('GET', 'API/clientes.php', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    var data = {usuario: user}
-    
-    request.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-        // Success!
-        var obj = JSON.parse(this.response);
-        obj.forEach(element => {
-            var body = document.querySelector('table tbody');
-            body.append(
-                "<tr>"+
-                    "<td>"+element.nombre+'</td>'+
-                "</tr>"
-            );
-        });
-        } else {
-            alert("An error");
-    
-        }
-    };
-
-    request.send(data);
-    
-    /*** Error: Uncaught SyntaxError: Unexpected end of JSON input
-     * at JSON.parse (<anonymous>) at XMLHttpRequest.request.onload
-     * 
-     * 
-     * The answer is in ajax-simple repository
-     */
+    // https://www.ma-no.org/en/programming/javascript/vanilla-javascript-equivalent-commands-to-jquery
 }
