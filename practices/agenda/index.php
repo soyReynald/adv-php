9cf4a33ec271e19165c89b04bf372c5d967fbb9b
Reynald Ramirez <soyreynald@outlook.com>
2020-12-07 21:20:43 -0400

    Setting up the environment for the Agenda

---
+++ practices/agenda/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar - events</title>
+    <style>
+        @import url('css/bootstrap.css');
+        @import url('css/fontello.css');
+        @import url('css/bootstrap-datepicker.css');
+    </style>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h3><i class="icon-calendar"></i>Calendar Events</h3>
+    </div>
+</body>
+</html>
