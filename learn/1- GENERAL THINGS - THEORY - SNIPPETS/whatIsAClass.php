bb34078085d2bb2cd3d62783b45a9cede2ab1874
Reynald Ramirez <reynaldramirez@hotmail.com>
2021-09-24 18:32:43 -0400

    What is an instance

---
+++ learn/1- GENERAL THINGS - THEORY - SNIPPETS/whatIsAClass.php
@@ -0,0 +1,16 @@
+<?php
+
+class Cars {
+    function color(){
+        echo "Red";
+    }
+}
+
+/*****
+ * The instances are what pays off (or declares) the class you are trying to access, but the object itself is what you can use to activate the methods inside of the class.
+ * 
+***/
+
+// A method inside of the class is actually a function, but is called method because is inside of the class.
+
+?>
