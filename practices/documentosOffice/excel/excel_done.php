c67ca7e0b016fa2a31dac471f961161546470537
Reynald Rodriguez <reynaldramirez@outlook.com>
2021-11-08 10:47:01 -0400

    Using the PHP to get excel documents to be downloaded

---
+++ practices/documentosOffice/excel/excel_done.php
@@ -0,0 +1,44 @@
+<?php
+
+require 'pdo.php';
+
+$query = "SELECT DATE_FORMAT(date, '%M %Y') AS month, SUM(amount) AS amount, SUM(profit) AS profit
+            FROM sales
+            GROUP BY month
+            ORDER BY date";
+$stmt = $pdo->prepare($query);
+
+$stmt->execute() or die(implode(' >> ', $stmt->errorInfo()));
+
+$sales = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header('Content-Type:application/pdf;charset=utf-8');
+header('Content-Disposition:attachment;filename=sheet.pdf');
+
+$header = false;
+$startRow = 2;
+$endRow = $stmt->rowCount() + 1;
+
+?>
+<?php if($stmt->rowCount() > 0): ?>
+<table border="2">
+    <?php foreach($sales as $sale): ?>
+        <?php if(!$header): ?>
+    <tr>
+        <th><?= implode('</th><th>', array_keys($sale)) ?></th>
+        <?php $header = true; ?>
+    </tr>
+    <?php endif ?>
+    <tr>
+        <td><?= $sale['month'] ?></td>
+        <td><?= $sale['amount'] ?></td>
+        <td><?= $sale['profit'] ?></td>
+    </tr>
+    <?php endforeach ?>
+    <tr>
+        <th>Total</th>
+        <td colspan="2"><?= "=SUM(B$startRow:B$endRow)" ?></td>
+        <td><?= "=SUM(C$startRow:C$endRow)" ?></td>
+    </tr>   
+</table>
+<?php endif ?>
---
+++ practices/documentosOffice/excel/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Generating documents</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container mt-1">
+        <h2>Exporting excel documents</h2>
+        <a href="excel_done.php" class="btn btn-outline-success">Excel</a>
+    </div>
+</body>
+
+</html> 
