8543cdea1c8a66203070ea2075ae93c98ce0ac7f
Reynald Rodriguez <reynaldramirez@outlook.com>
2021-11-25 17:21:19 -0400

    [U] Updates on fakebook

--- practices/fakebook/app/views/default.php
+++ practices/fakebook/app/views/default.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,9 +11,23 @@
     <script src="public/js/fontawesome.js"></script>
     <title>Fakebook</title>
 </head>
+
 <body>
     <div class="container">
-        <h2><?= $data['company'] ?></h2>
+        <!-- <h2><i class="fas fa-car"></i><?= $data['company'] ?></h2> -->
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <form action="">
+                        <div class="form-group"><input class="form-control" type="text" name="user"  placeholder="_USER_"></div>
+                        <div class="form-group"><input class="form-control" type="password" name="password" placeholder="_PASSWORD_"></div>
+                        <button class="btn btn-primary"><?= '_LOGIN_' ?></button>
+                    </form>
+                </div>
+                <div class="col-9">Right</div>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
