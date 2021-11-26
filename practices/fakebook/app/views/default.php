aed9897794462f94784600579d534c61cdf664d4
Reynald Rodriguez <reynaldramirez@outlook.com>
2021-11-26 11:09:02 -0400

    Updates in globals.css

--- practices/fakebook/app/views/default.php
+++ practices/fakebook/app/views/default.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/fontawesome.css">
+    <link rel="stylesheet" href="public/css/globals.css">
     <script src="public/js/bootstrap.js"></script>
     <script src="public/js/fontawesome.js"></script>
     <title>Fakebook</title>
