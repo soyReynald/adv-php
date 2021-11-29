42aa0a538f276b0184c6fe212992a2dae8108c95
Reynald Rodriguez <reynaldramirez@outlook.com>
2021-11-29 17:24:09 -0400

    [U] Updates in default styling

--- practices/fakebook/app/views/default.php
+++ practices/fakebook/app/views/default.php
@@ -20,9 +20,20 @@
             <div class="row">
                 <div class="col-3">
                     <form action="">
-                        <div class="form-group"><input class="form-control" type="text" name="user"  placeholder="_USER_"></div>
-                        <div class="form-group"><input class="form-control" type="password" name="password" placeholder="_PASSWORD_"></div>
-                        <button class="btn btn-primary"><?= '_LOGIN_' ?></button>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user" style="font-size: 24px;"></i></span>
+                            </div>
+                            <input class="form-control" type="text" name="user" placeholder="_USER_">
+                        </div>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text py-2"><i class="fas fa-key" style="font-size: 24px;"></i></span>
+                            </div>
+                            <input class="form-control" type="password" name="password" placeholder="_PASSWORD_">
+                        </div>
+                        <button class="btn btn-primary float-end"><?= '_LOGIN_' ?></button>
+                        <a href="?url=createAccount" class="btn btn-link"><?= '_CREATE ACCOUNT_' ?></a>
                     </form>
                 </div>
                 <div class="col-9">Right</div>
