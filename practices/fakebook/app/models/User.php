c90f41683d6fe6247bf38351db6a43bb73323b02
Reynald Rodriguez <reynaldramirez@outlook.com>
2021-12-15 17:57:53 -0400

    [U] Cleaning variables and to fakebook

--- practices/fakebook/app/models/User.php
+++ practices/fakebook/app/models/User.php
@@ -2,11 +2,18 @@
 
 class User extends Model
 {
-    public function __construct($name, $username, $email, $password) {
+    public function __construct ($name, $username, $email, $password) {
         parent::__construct();
+        $password = password_hash($password, PASSWORD_BCRYPT);
+        $name = $this->cleanText($name);
+        $username = $this->cleanText($username);
+        $email = $this->cleanText($email);
+        $token = md5($email);
         $sql = "INSERT INTO users (name, user, email, password) VALUES ('$name', '$username', '$email', '$password')";
         return $this->query($sql);
     }
-
+    private function cleanText ($text) {
+        return $this->real_escape_string(strip_tags($text));
+    }
 }
 ?>
